766654d10747bf368332ec522463b0e0d5bb36f8
adhish1478 <adhish.aravind7@gmail.com>
2025-06-21 04:22:44 +0530

    routing moved to poll app

--- pollify/asgi.py
+++ pollify/asgi.py
@@ -12,17 +12,19 @@ import django
 from django.core.asgi import get_asgi_application
 from channels.routing import get_default_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pollify.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pollify.settings")
 django.setup()
 
 
 # Import necessary modules for Channels
+import polls.routing
 from channels.routing import ProtocolTypeRouter, URLRouter
-from pollify.routing import websocket_urlpatterns
+from polls.routing import websocket_urlpatterns
 from channels.auth import AuthMiddlewareStack
 # Create the ASGI application
 application= ProtocolTypeRouter({
+    "http": get_asgi_application(),  # Handle HTTP requests
     "websocket" : AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
-    )
+        URLRouter(polls.routing.websocket_urlpatterns)
+    ),
 })
